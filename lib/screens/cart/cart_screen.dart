724672a6eacbd612624724b538ffd0239a1e7766
phong.bnh <phong.bnh@globalfactories.com>
2022-08-13 11:41:51 +0700

    fea(cart): update UI screen

--- lib/screens/cart/cart_screen.dart
+++ lib/screens/cart/cart_screen.dart
@@ -1,5 +1,7 @@
+import 'package:ecommerce_bloc/models/product_model.dart';
 import 'package:flutter/material.dart';
 
+import '../../widgets/cart_product_card.dart';
 import '../../widgets/widgets.dart';
 
 class CartScreen extends StatelessWidget {
@@ -18,7 +20,153 @@ class CartScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: CustomAppBar(title: 'Cart'),
-      bottomNavigationBar: CustomBottomBar(),
+      bottomNavigationBar: BottomAppBar(
+        color: Colors.black,
+        child: SizedBox(
+          height: 70,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(primary: Colors.white),
+                onPressed: () {},
+                child: Text('go to checkout'.toUpperCase(),
+                    style: Theme.of(context).textTheme.headline3),
+              ),
+            ],
+          ),
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
+        child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        'Add \$20.0 for FREE Delivery',
+                        style: Theme.of(context).textTheme.headline5,
+                      ),
+                      ElevatedButton(
+                        onPressed: () {
+                          Navigator.pushNamed(context, '/');
+                        },
+                        style: ElevatedButton.styleFrom(
+                            primary: Colors.black,
+                            shape: RoundedRectangleBorder(),
+                            elevation: 0),
+                        child: Text(
+                          'Add more item',
+                          style: Theme.of(context)
+                              .textTheme
+                              .headline5!
+                              .copyWith(color: Colors.white),
+                        ),
+                      )
+                    ],
+                  ),
+                  const SizedBox(
+                    height: 10.0,
+                  ),
+                  CartProductCard(
+                    product: Product.products[0],
+                  ),
+                  CartProductCard(
+                    product: Product.products[1],
+                  ),
+                ],
+              ),
+              Column(
+                children: [
+                  Divider(
+                    thickness: 2,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 40, vertical: 10),
+                    child: Column(
+                      children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text(
+                              'subtotal'.toUpperCase(),
+                              style: Theme.of(context).textTheme.headline5,
+                            ),
+                            Text(
+                              '\$5.98',
+                              style: Theme.of(context).textTheme.headline5,
+                            )
+                          ],
+                        ),
+                        const SizedBox(
+                          height: 10,
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text(
+                              'delivery fee'.toUpperCase(),
+                              style: Theme.of(context).textTheme.headline5,
+                            ),
+                            Text(
+                              '\$2.90',
+                              style: Theme.of(context).textTheme.headline5,
+                            )
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                  Stack(
+                    children: [
+                      Container(
+                        width: MediaQuery.of(context).size.width,
+                        height: 60,
+                        decoration: BoxDecoration(
+                          color: Colors.black.withAlpha(50),
+                        ),
+                      ),
+                      Container(
+                        width: MediaQuery.of(context).size.width,
+                        height: 50,
+                        margin: const EdgeInsets.all(5.0),
+                        decoration: BoxDecoration(
+                          color: Colors.black,
+                        ),
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 30.0),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Text(
+                                'total'.toUpperCase(),
+                                style: Theme.of(context)
+                                    .textTheme
+                                    .headline5!
+                                    .copyWith(color: Colors.white),
+                              ),
+                              Text(
+                                '\$5.98',
+                                style: Theme.of(context)
+                                    .textTheme
+                                    .headline5!
+                                    .copyWith(color: Colors.white),
+                              )
+                            ],
+                          ),
+                        ),
+                      )
+                    ],
+                  )
+                ],
+              )
+            ]),
+      ),
     );
   }
 }
